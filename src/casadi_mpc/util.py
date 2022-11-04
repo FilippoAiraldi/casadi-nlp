6683a840b92d2752d54c09439e05e3618de7f8e6
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-11-04 22:30:05 +0100

    implemented cached property resetter

--- src/casadi_mpc/util.py
+++ src/casadi_mpc/util.py
@@ -1,4 +1,5 @@
-from typing import Any
+from functools import cached_property, wraps
+from typing import Any, Callable
 import casadi as cs
 
 
@@ -22,3 +23,39 @@ def is_casadi_object(obj: Any) -> bool:
         return False
     module: str = obj.__module__.split('.')[0]
     return module == cs.__name__
+
+
+def cached_property_reset(cproperty: cached_property) -> Callable:
+    '''Decorator that allows to enhance a method with the ability, when called,
+    to clear the cached of a target property. This is especially useful to 
+    reset the cache of a given cached property when a method makes changes to 
+    the underlying data, thus compromising the cached results.
+
+    Returns
+    -------
+    decorated_func : Callable
+        Returns the function wrapped with this decorator.
+
+    Raises
+    ------
+    TypeError
+        Raises if the given property is not an instance of 
+        `functools.cached_property`.
+    '''
+
+    if not isinstance(cproperty, cached_property):
+        raise TypeError(
+            'The specified property must be an instance of '
+            f'`functools.cached_property`; got {type(cproperty)} instead.')
+
+    # use a double decorator as it is a trick to allow passing arguments to it
+    def actual_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            self = args[0]
+            n = cproperty.attrname
+            if n is not None and n in self.__dict__:
+                del self.__dict__[n]
+            return func(*args, **kwargs)
+        return wrapper
+    return actual_decorator
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,9 +1,41 @@
 import unittest
+from functools import cached_property
 import casadi as cs
-from casadi_mpc.util import is_casadi_object
+from casadi_mpc.util import is_casadi_object, cached_property_reset
+
+
+class DummyWithCachedProperty:
+    def __init__(self) -> None:
+        self.counter = 0
+
+    @cached_property
+    def a_cached_property(self) -> int:
+        self.counter += 1
+        return self.counter
+
+    @cached_property_reset(a_cached_property)
+    def clear_cache(self) -> None:
+        return
 
 
 class TestUtil(unittest.TestCase):
+    def test_cached_property_reset__raises__with_invalid_type(self):
+        with self.assertRaises(TypeError):
+            cached_property_reset(5)
+
+    def test_cached_property_reset__clears_property_cache(self):
+        dummy = DummyWithCachedProperty()
+        dummy.a_cached_property
+        dummy.a_cached_property
+        self.assertEqual(dummy.counter, 1)
+        dummy.clear_cache()
+        dummy.a_cached_property
+        self.assertEqual(dummy.counter, 2)
+        dummy.clear_cache()
+        dummy.a_cached_property
+        dummy.a_cached_property
+        self.assertEqual(dummy.counter, 3)
+
     def test_is_casadi_object__guesses_correctly(self):
         for o, flag in [
             (5.0, False),
