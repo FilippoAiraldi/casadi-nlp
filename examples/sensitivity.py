05ee460bce52d60fc49409ae599cf9c0d868d2f4
Filippo Airaldi <filippoairaldi@gmail.com>
2022-11-09 16:11:52 +0100

    rewritten with double pars

--- examples/sensitivity.py
+++ examples/sensitivity.py
@@ -41,13 +41,12 @@ def z(x, lam):
 nlp = Nlp(sym_type='SX')
 
 x = nlp.variable('x', (2, 1), lb=[[0], [-np.inf]])[0]
-a = 0.2
-p = nlp.parameter('p')
+p = nlp.parameter('p', (2, 1))
 
-nlp.minimize((1 - x[0])**2 + a * (x[1] - x[0]**2)**2)
+nlp.minimize((1 - x[0])**2 + p[0] * (x[1] - x[0]**2)**2)
 g = (x[0] + 0.5)**2 + x[1]**2
-nlp.constraint('c1', (p / 2)**2, '<=', g)
-_, lam = nlp.constraint('c2', g, '<=', p**2)
+nlp.constraint('c1', (p[1] / 2)**2, '<=', g)
+_, lam = nlp.constraint('c2', g, '<=', p[1]**2)
 opts = {'print_time': False, 'ipopt': {'sb': 'yes', 'print_level': 0}}
 nlp.init_solver(opts)
 
@@ -59,16 +58,16 @@ p_values = (1.2, 1.5, 1.9)
 fig, axs = plt.subplots(
     1, 3, sharey=True, constrained_layout=True, figsize=(8, 3))
 for p0, ax in zip(p_values, axs):
-    x_ = M(p0, 0).full()
-    plot_nlp(ax, a, p0, x_[0], x_[1])
+    x_ = M([0.2, p0], 0).full()
+    plot_nlp(ax, 0.2, p0, x_[0], x_[1])
 
 
 # How does the optimal solution vary along p?
 Z = nlp.to_function('Z', [p, x], [z(x, lam)], ['p', 'x0'], ['z'])
 
 fig, ax = plt.subplots(constrained_layout=True)
-pv = np.linspace(1, 2, 100).reshape(1, -1)
-ax.plot(pv.flat, Z(pv, 0).full().flat, 'k.', markersize=8)
+pv = np.row_stack((np.full(100, 0.2), np.linspace(1, 2, 100)))
+ax.plot(pv[1].flat, Z(pv, 0).full().flat, 'k.', markersize=8)
 
 
 # Parametric sensitivities
@@ -80,14 +79,14 @@ dzdy = cs.jacobian(z(x, lam), y).T
 
 t = np.linspace(1, 2, 1000)
 for p0, clr in zip(p_values, ['r', 'g', 'b']):
-    sol = nlp.solve(pars={'p': p0})
+    sol = nlp.solve(pars={'p': [0.2, p0]})
     F = sol.value(z(x, lam))
-    J = sol.value(dzdy.T @ dydp)
+    J = sol.value(dzdy.T @ dydp)[1]  # take jacobian of only the 2nd parameter
     H = 0  # 3.5169, 1.4018, 0
 
     ax.plot(p0, float(F), 'x', color=clr, markersize=16)
     ax.plot(t, F + J * (t - p0) + 0.5 * H * (t - p0)**2, lw=2, color=clr)
 
-ax.set_xlim(t[0], t[-1])
+ax.set_xlim(1, 2)
 ax.set_ylim(-0.17, 0.03)
 plt.show()
