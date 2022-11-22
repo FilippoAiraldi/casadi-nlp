5672ee4f6fd6ec04fee5cd057e2c59d54969a954
Filippo Airaldi <filippoairaldi@gmail.com>
2022-11-22 15:00:07 +0100

    implemented example for multistart nlp

---
+++ examples/multistart_nlp.py
@@ -0,0 +1,63 @@
+from typing import List
+import numpy as np
+import matplotlib.pyplot as plt
+from csnlp import MultistartNlp
+from csnlp.solutions import Solution
+
+
+def func(x):
+    return -0.3 * x**2 - np.exp(-10 * x**2) + np.exp(-100 * (x - 1)**2) + \
+        np.exp(-100 * (x - 1.5)**2)
+
+
+# build the NLP
+N = 3
+LB, UB = -0.5, 1.4
+nlp = MultistartNlp(starts=N)
+x = nlp.variable('x', lb=LB, ub=UB)[0]
+nlp.parameter('p0')
+nlp.parameter('p1')
+nlp.minimize(func(x))
+opts = {'print_time': False, 'ipopt': {'sb': 'yes', 'print_level': 0}}
+nlp.init_solver(opts)
+
+# manually solve the problem from multiple initial conditions
+x0s = [0.9, 0.5, 1.1]
+xfs = [float(nlp.solve(pars={'p0': 0, 'p1': 1}, vals0={'x': x0}).vals['x'])
+       for x0 in x0s]
+
+# use automatical multistart solver
+args = ([{'p0': 0, 'p1': 1} for _ in x0s], [{'x': x0} for x0 in x0s])
+best_sol: Solution = nlp.solve_multi(*args)
+all_sols: List[Solution] = nlp.solve_multi(*args, return_all_sols=True)
+
+# plotting
+fig, ax = plt.subplots(constrained_layout=True)
+xs = np.linspace(LB, UB, 500)
+ax.plot(xs, func(xs), 'k--')
+
+# manual solutions
+for x0, xf in zip(x0s, xfs):
+    xs = np.linspace(x0, xf, 100)
+    lbl = fr'$x_0={{{x0:.1f}}} \rightarrow f^{{\star}}={{{func(xf):.2f}}}$'
+    c = ax.plot(xs, func(xs), '-', lw=2, label=lbl)[0]
+    ax.plot(x0, func(x0), 'o', markersize=6, color=c.get_color())
+    ax.plot(xf, func(xf), '*', markersize=8, color=c.get_color())
+
+# all multistart solutions
+for sol in all_sols:
+    ax.plot(sol.vals['x'], sol.f, 'ko', markersize=12, fillstyle='none')
+
+# best solution
+ax.plot(best_sol.value(x), best_sol.value(nlp.f), 'rs', markersize=14,
+        fillstyle='none')
+x = float(best_sol.vals['x'])
+ax.vlines(x, -1.1, best_sol.f, 'r', ls='-.')
+ax.hlines(best_sol.f, LB, x, 'r', ls='-.')
+
+ax.set_xlabel('x')
+ax.set_ylabel('f(x)')
+ax.set_xlim(LB, UB)
+ax.set_ylim(-1.1, 0.8)
+ax.legend()
+plt.show()
