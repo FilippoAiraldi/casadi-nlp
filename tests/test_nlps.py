2121014e22faf8d4e2e3ed26ab0263c8ca5be15c
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-03-29 16:27:19 +0200

    fixed tests for new masked lbx and ubx

--- tests/test_nlps.py
+++ tests/test_nlps.py
@@ -37,7 +37,7 @@ class TestNlp(unittest.TestCase):
             rhs = np.full(lhs.shape, rhs)
         if not force_numerical and self.sym_type == "SX":
             return cs.is_equal(lhs, rhs)
-        old = cs.vertcat(*(cs.vec(o) for o in vars))
+        old = cs.vvcat(vars)
         new = np.random.randn(*old.shape)
         np.testing.assert_allclose(subsevalf(lhs, old, new), subsevalf(rhs, old, new))
 
@@ -84,23 +84,27 @@ class TestNlp(unittest.TestCase):
     def test_variable__creates_correct_variable(self):
         shape1 = (4, 3)
         shape2 = (2, 2)
+        nx1, nx2 = np.prod(shape1), np.prod(shape2)
+        nx = nx1 + nx2
         lb1, ub1 = np.random.rand(*shape1) - 1, np.random.rand(*shape1) + 1
         lb2, ub2 = np.random.rand(*shape2) - 1, np.random.rand(*shape2) + 1
-        nx = np.prod(shape1) + np.prod(shape2)
         nlp = Nlp(sym_type=self.sym_type, debug=True)
         x1, lam1_lb, lam1_ub = nlp.variable("x1", shape1, lb=lb1, ub=ub1)
         x2, lam2_lb, lam2_ub = nlp.variable("x2", shape2, lb=lb2, ub=ub2)
-        for o in (x1, lam1_lb, lam1_ub):
-            self.assertEqual(o.shape, shape1)
-        for o in (x2, lam2_lb, lam2_ub):
-            self.assertEqual(o.shape, shape2)
+
+        self.assertEqual(x1.shape, shape1)
+        self.assertEqual(lam1_lb.shape, (nx1, 1))
+        self.assertEqual(lam1_ub.shape, (nx1, 1))
+        self.assertEqual(x2.shape, shape2)
+        self.assertEqual(lam2_lb.shape, (nx2, 1))
+        self.assertEqual(lam2_ub.shape, (nx2, 1))
         self.assertEqual(nlp.nx, nx)
 
-        x = cs.vertcat(cs.vec(x1), cs.vec(x2))
+        x = cs.veccat(x1, x2)
         self.cmp(nlp.x, x, vars=[x1, x2])
 
-        lb = cs.vertcat(cs.vec(lb1), cs.vec(lb2))
-        ub = cs.vertcat(cs.vec(ub1), cs.vec(ub2))
+        lb = cs.veccat(lb1, lb2)
+        ub = cs.veccat(ub1, ub2)
         np.testing.assert_allclose(nlp.lbx, lb.full().flat)
         np.testing.assert_allclose(nlp.ubx, ub.full().flat)
 
@@ -162,15 +166,18 @@ class TestNlp(unittest.TestCase):
     @parameterized.expand([("==", "=="), ("==", ">="), (">=", ">=")])
     def test_constraint__creates_constraint_correctly(self, op1: str, op2: str):
         shape1, shape2 = (4, 3), (2, 2)
-        nc = np.prod(shape1) + np.prod(shape2)
+        nc1, nc2 = np.prod(shape1), np.prod(shape2)
+        nc = nc1 + nc2
         nlp = Nlp(sym_type=self.sym_type, debug=True)
         x = nlp.variable("x", shape1)[0]
         y = nlp.variable("y", shape2)[0]
         c1, lam_c1 = nlp.constraint("c1", x, op1, 5)
         c2, lam_c2 = nlp.constraint("c2", 5, op2, y)
 
-        self.assertTrue(c1.shape == lam_c1.shape == shape1)
-        self.assertTrue(c2.shape == lam_c2.shape == shape2)
+        self.assertEqual(c1.shape, shape1)
+        self.assertEqual(lam_c1.shape, (nc1, 1))
+        self.assertEqual(c2.shape, shape2)
+        self.assertEqual(lam_c2.shape, (nc2, 1))
         self.assertEqual(nlp.ng + nlp.nh, nc)
 
         i, prev_op = 0, op1
@@ -186,8 +193,8 @@ class TestNlp(unittest.TestCase):
             with self.assertRaises(IndexError):
                 describe(nc + 1)
 
-        c = cs.vertcat(cs.vec(c1), cs.vec(c2))
-        lam = cs.vertcat(cs.vec(lam_c1), cs.vec(lam_c2))
+        c = cs.veccat(c1, c2)
+        lam = cs.vertcat(lam_c1, lam_c2)
         expected_c = cs.vertcat(nlp.g, nlp.h)
         expected_lam = cs.vertcat(nlp.lam_g, nlp.lam_h)
         self.cmp(expected_c, c, vars=[x, y])
@@ -270,149 +277,96 @@ class TestNlp(unittest.TestCase):
         x, lam_lbx, lam_ubx = nlp.variable("x", (2, 3), lb=[[0], [-np.inf]], ub=1)
         _, lam_g = nlp.constraint("c1", x[:, 0], "==", 2)
         _, lam_h = nlp.constraint("c2", x[0, :] + x[1, :] ** 2, "<=", 2)
-        v = cs.vec
-        vc = cs.vertcat
 
-        lam = vc(v(lam_g), v(lam_h), v(lam_lbx[0, :]), v(lam_ubx))
-        lam_all = vc(v(lam_g), v(lam_h), v(lam_lbx), v(lam_ubx))
-        y = vc(v(x), v(lam_g), v(lam_h), v(lam_lbx[0, :]), v(lam_ubx))
-        y_all = vc(v(x), v(lam_g), v(lam_h), v(lam_lbx), v(lam_ubx))
+        lam = cs.vertcat(lam_g, lam_h, lam_lbx, lam_ubx)
+        y = cs.veccat(x, lam_g, lam_h, lam_lbx, lam_ubx)
 
         vars = [x, lam_g, lam_h, lam_lbx, lam_ubx]
         self.cmp(nlp.lam, lam, vars=vars)
-        self.cmp(nlp.lam_all, lam_all, vars=vars)
-        self.cmp(nlp.primal_dual_vars(all=False), y, vars=vars)
-        self.cmp(nlp.primal_dual_vars(all=True), y_all, vars=vars)
+        self.cmp(nlp.primal_dual, y, vars=vars)
 
     @parameterized.expand([(True,), (False,)])
-    def test_h_lbx_ubx__returns_correct_indices(self, flag: bool):
-        # sourcery skip: low-code-quality
-        nlp = Nlp(sym_type=self.sym_type, remove_redundant_x_bounds=flag)
+    def test_h_lbx_ubx__returns_correct_indices(self, remove_red_bnds: bool):
+        nlp = Nlp(sym_type=self.sym_type, remove_redundant_x_bounds=remove_red_bnds)
 
-        x1, lam_lbx1, lam_ubx1 = nlp.variable("x1", (2, 1))
-        (h_lbx, lam_lbx), (h_ubx, lam_ubx) = nlp.h_lbx, nlp.h_ubx
-        vars = [x1, lam_lbx1, lam_ubx1]
-        if flag:
-            self.assertTrue(all(o.is_empty() for o in [h_lbx, lam_lbx, h_ubx, lam_ubx]))
-        else:
-            if self.sym_type == "SX":
-                self.cmp((h_lbx - (-np.inf - x1)), 0, vars=vars, force_numerical=True)
-                self.cmp((h_ubx - (x1 - np.inf)), 0, vars=vars, force_numerical=True)
-            self.cmp(cs.evalf(lam_lbx - lam_lbx1), 0, vars=vars)
-            self.cmp(cs.evalf(lam_ubx - lam_ubx1), 0, vars=vars)
+        x1, lam_lbx_c1, lam_ubx_c1 = nlp.variable("x1", (2, 1))
+        lam_lbx1, lam_ubx1 = nlp.lam_lbx, nlp.lam_ubx
+        h_lbx1, h_ubx1 = nlp.h_lbx, nlp.h_ubx
+        vars1 = [x1, lam_lbx_c1, lam_ubx_c1]
+        #
+        x2, lam_lbx_c2, lam_ubx_c2 = nlp.variable("x2", (2, 1), lb=0)
+        lam_lbx2, lam_ubx2 = nlp.lam_lbx, nlp.lam_ubx
+        h_lbx2, h_ubx2 = nlp.h_lbx, nlp.h_ubx
+        vars2 = vars1.copy() + [x2, lam_lbx_c2, lam_ubx_c2]
+        #
+        x3, lam_lbx_c3, lam_ubx_c3 = nlp.variable("x3", (2, 1), ub=1)
+        lam_lbx3, lam_ubx3 = nlp.lam_lbx, nlp.lam_ubx
+        h_lbx3, h_ubx3 = nlp.h_lbx, nlp.h_ubx
+        vars3 = vars2.copy() + [x3, lam_lbx_c3, lam_ubx_c3]
+        #
+        x4, lam_lbx_c4, lam_ubx_c4 = nlp.variable("x4", (2, 1), lb=0, ub=1)
+        lam_lbx4, lam_ubx4 = nlp.lam_lbx, nlp.lam_ubx
+        h_lbx4, h_ubx4 = nlp.h_lbx, nlp.h_ubx
+        vars4 = vars3.copy() + [x4, lam_lbx_c4, lam_ubx_c4]
 
-        x2, lam_lbx2, lam_ubx2 = nlp.variable("x2", (2, 1), lb=0)
-        (h_lbx, lam_lbx), (h_ubx, lam_ubx) = nlp.h_lbx, nlp.h_ubx
-        vars.extend([x2, lam_lbx2, lam_ubx2])
-        if flag:
-            self.assertTrue(all(o.is_empty() for o in [h_ubx, lam_ubx]))
-            self.cmp(cs.evalf(h_lbx - (-x2)), 0, vars=vars)
-            self.cmp(cs.evalf(lam_lbx - lam_lbx2), 0, vars=vars)
+        if remove_red_bnds:
+            self.assertTrue(
+                all(o.is_empty() for o in [h_lbx1, lam_lbx1, h_ubx1, lam_ubx1])
+            )
+            #
+            self.assertTrue(all(o.is_empty() for o in [h_ubx2, lam_ubx2]))
+            self.cmp(cs.evalf(h_lbx2 - (-x2)), 0, vars=vars2)
+            self.cmp(cs.evalf(lam_lbx2 - lam_lbx_c2), 0, vars=vars2)
+            #
+            self.cmp(cs.evalf(h_lbx3 - (-x2)), 0, vars=vars3)
+            self.cmp(cs.evalf(lam_lbx3 - lam_lbx2), 0, vars=vars3)
+            self.cmp((h_ubx3 - (x3 - 1)), 0, vars=vars3, force_numerical=True)
+            self.cmp(cs.evalf(lam_ubx3 - lam_ubx_c3), 0, vars=vars3)
+            #
+            self.cmp(h_lbx4 - cs.vertcat(-x2, -x4), 0, vars=vars4, force_numerical=True)
+            e = lam_lbx4 - cs.vertcat(lam_lbx_c2, lam_lbx_c4)
+            self.cmp(e, 0, vars=vars4, force_numerical=True)
+            e = h_ubx4 - cs.vertcat(x3 - 1, x4 - 1)
+            self.cmp(e, 0, vars=vars4, force_numerical=True)
+            e = lam_ubx4 - cs.vertcat(lam_ubx_c3, lam_ubx_c4)
+            self.cmp(e, 0, vars=vars4, force_numerical=True)
         else:
-            if self.sym_type == "SX":
-                self.cmp(
-                    cs.evalf(h_lbx - cs.vertcat(-np.inf - x1, 0 - x2)),
-                    0,
-                    vars=vars,
-                )
-                self.cmp(
-                    cs.evalf(h_ubx - cs.vertcat(x1 - np.inf, x2 - np.inf)),
-                    0,
-                    vars=vars,
-                )
-            self.cmp(
-                (lam_lbx - cs.vertcat(lam_lbx1, lam_lbx2)),
-                0,
-                vars=vars,
-                force_numerical=True,
-            )
-            self.cmp(
-                (lam_ubx - cs.vertcat(lam_ubx1, lam_ubx2)),
-                0,
-                vars=vars,
-            )
+            self.cmp(cs.evalf(lam_lbx1 - lam_lbx_c1), 0, vars=vars1)
+            self.cmp(cs.evalf(lam_ubx1 - lam_ubx_c1), 0, vars=vars1)
+            #
+            e = lam_lbx2 - cs.vertcat(lam_lbx_c1, lam_lbx_c2)
+            self.cmp(e, 0, vars=vars2, force_numerical=True)
+            e = lam_ubx2 - cs.vertcat(lam_ubx_c1, lam_ubx_c2)
+            self.cmp(e, 0, vars=vars2)
+            #
+            e = lam_lbx3 - cs.vertcat(lam_lbx_c1, lam_lbx_c2, lam_lbx_c3)
+            self.cmp(e, 0, vars=vars3, force_numerical=True)
+            e = lam_ubx3 - cs.vertcat(lam_ubx_c1, lam_ubx_c2, lam_ubx_c3)
+            self.cmp(e, 0, vars=vars3, force_numerical=True)
+            #
+            e = lam_lbx4 - cs.vertcat(lam_lbx_c1, lam_lbx_c2, lam_lbx_c3, lam_lbx_c4)
+            self.cmp(e, 0, vars=vars4, force_numerical=True)
+            e = lam_ubx4 - cs.vertcat(lam_ubx_c1, lam_ubx_c2, lam_ubx_c3, lam_ubx_c4)
+            self.cmp(e, 0, vars=vars4, force_numerical=True)
 
-        x3, lam_lbx3, lam_ubx3 = nlp.variable("x3", (2, 1), ub=1)
-        (h_lbx, lam_lbx), (h_ubx, lam_ubx) = nlp.h_lbx, nlp.h_ubx
-        vars.extend([x3, lam_lbx3, lam_ubx3])
-        if flag:
-            self.cmp(cs.evalf(h_lbx - (-x2)), 0, vars=vars)
-            self.cmp(cs.evalf(lam_lbx - lam_lbx2), 0, vars=vars)
-            self.cmp((h_ubx - (x3 - 1)), 0, vars=vars, force_numerical=True)
-            self.cmp(cs.evalf(lam_ubx - lam_ubx3), 0, vars=vars)
-        else:
             if self.sym_type == "SX":
-                self.cmp(
-                    cs.evalf(h_lbx - cs.vertcat(-np.inf - x1, 0 - x2, -np.inf - x3)),
-                    0,
-                    vars=vars,
-                )
-                self.cmp(
-                    cs.evalf(h_ubx - cs.vertcat(x1 - np.inf, x2 - np.inf, x3 - 1)),
-                    0,
-                    vars=vars,
-                )
-            self.cmp(
-                (lam_lbx - cs.vertcat(lam_lbx1, lam_lbx2, lam_lbx3)),
-                0,
-                vars=vars,
-                force_numerical=True,
-            )
-            self.cmp(
-                (lam_ubx - cs.vertcat(lam_ubx1, lam_ubx2, lam_ubx3)),
-                0,
-                vars=vars,
-                force_numerical=True,
-            )
-
-        x4, lam_lbx4, lam_ubx4 = nlp.variable("x4", (2, 1), lb=0, ub=1)
-        (h_lbx, lam_lbx), (h_ubx, lam_ubx) = nlp.h_lbx, nlp.h_ubx
-        vars.extend([x4, lam_lbx4, lam_ubx4])
-        if flag:
-            self.cmp((h_lbx - cs.vertcat(-x2, -x4)), 0, vars=vars, force_numerical=True)
-            self.cmp(
-                (lam_lbx - cs.vertcat(lam_lbx2, lam_lbx4)),
-                0,
-                vars=vars,
-                force_numerical=True,
-            )
-            self.cmp(
-                (h_ubx - cs.vertcat(x3 - 1, x4 - 1)), 0, vars=vars, force_numerical=True
-            )
-            self.cmp(
-                (lam_ubx - cs.vertcat(lam_ubx3, lam_ubx4)),
-                0,
-                vars=vars,
-                force_numerical=True,
-            )
-        else:
-            if self.sym_type == "SX":
-                self.cmp(
-                    cs.evalf(
-                        h_lbx - cs.vertcat(-np.inf - x1, 0 - x2, -np.inf - x3, 0 - x4)
-                    ),
-                    0,
-                    vars=vars,
-                )
-                self.cmp(
-                    cs.evalf(
-                        h_ubx - cs.vertcat(x1 - np.inf, x2 - np.inf, x3 - 1, x4 - 1)
-                    ),
-                    0,
-                    vars=vars,
-                )
-            self.cmp(
-                (lam_lbx - cs.vertcat(lam_lbx1, lam_lbx2, lam_lbx3, lam_lbx4)),
-                0,
-                vars=vars,
-                force_numerical=True,
-            )
-            self.cmp(
-                (lam_ubx - cs.vertcat(lam_ubx1, lam_ubx2, lam_ubx3, lam_ubx4)),
-                0,
-                vars=vars,
-                force_numerical=True,
-            )
+                self.cmp(h_lbx1 - (-np.inf - x1), 0, vars=vars1, force_numerical=True)
+                self.cmp(h_ubx1 - (x1 - np.inf), 0, vars=vars1, force_numerical=True)
+                #
+                e = h_lbx2 - cs.vertcat(-np.inf - x1, 0 - x2)
+                self.cmp(cs.evalf(e), 0, vars=vars2)
+                e = h_ubx2 - cs.vertcat(x1 - np.inf, x2 - np.inf)
+                self.cmp(cs.evalf(e), 0, vars=vars2)
+                #
+                e = h_lbx3 - cs.vertcat(-np.inf - x1, 0 - x2, -np.inf - x3)
+                self.cmp(cs.evalf(e), 0, vars=vars3)
+                e = h_ubx3 - cs.vertcat(x1 - np.inf, x2 - np.inf, x3 - 1)
+                self.cmp(cs.evalf(e), 0, vars=vars3)
+                #
+                e = h_lbx4 - cs.vertcat(-np.inf - x1, 0 - x2, -np.inf - x3, 0 - x4)
+                self.cmp(cs.evalf(e), 0, vars=vars4)
+                e = h_ubx4 - cs.vertcat(x1 - np.inf, x2 - np.inf, x3 - 1, x4 - 1)
+                self.cmp(cs.evalf(e), 0, vars=vars4)
 
     def test_init_solver__raises__when_objective_not_set(self):
         nlp = Nlp(sym_type=self.sym_type)
@@ -458,7 +412,7 @@ class TestNlp(unittest.TestCase):
         np.testing.assert_allclose(sol.f, o)
 
     def test_solve__computes_corretly__example_1a(self):
-        # https://en.wikipedia.org/wiki/Lagrange_multiplier#Example_1a
+        # https://en.wikipedia.org/wiki/Lagrange_multiplier#Example_1
         nlp = Nlp(sym_type=self.sym_type)
         x = nlp.variable("x")[0]
         y = nlp.variable("y")[0]
@@ -472,7 +426,7 @@ class TestNlp(unittest.TestCase):
         np.testing.assert_allclose(sol.value(nlp.lam_g), 1 / np.sqrt(2), atol=1e-9)
 
     def test_solve__computes_corretly__example_1b(self):
-        # https://en.wikipedia.org/wiki/Lagrange_multiplier#Example_1b
+        # https://en.wikipedia.org/wiki/Lagrange_multiplier#Example_1
         nlp = Nlp(sym_type=self.sym_type)
         x = nlp.variable("x")[0]
         y = nlp.variable("y")[0]
@@ -502,7 +456,7 @@ class TestNlp(unittest.TestCase):
         )
 
     def test_solve__computes_corretly__example_3(self):
-        # https://en.wikipedia.org/wiki/Lagrange_multiplier#Example_3:_Entropy
+        # https://en.wikipedia.org/wiki/Lagrange_multiplier#Example_3
         n = 50
         nlp = Nlp(sym_type=self.sym_type)
         p = nlp.variable("p", (n, 1), lb=1 / n * 1e-6)[0]
@@ -516,7 +470,7 @@ class TestNlp(unittest.TestCase):
         )
 
     def test_solve__computes_corretly__example_4(self):
-        # https://en.wikipedia.org/wiki/Lagrange_multiplier#Example_4:_Numerical_optimization
+        # https://en.wikipedia.org/wiki/Lagrange_multiplier#Example_4
         nlp = Nlp(sym_type=self.sym_type)
         x = nlp.variable("x")[0]
         nlp.constraint("c1", x**2, "==", 1)
