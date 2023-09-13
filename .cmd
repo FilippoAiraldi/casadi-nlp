9b2fe5dab7271fb2fb50c3ef56b03cfdfdbe8635
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-09-13 15:59:03 +0200

    small modifcation to config file
    (cherry picked from commit bd8e41a1cf2405489f0579d98fa5fe71f2136d59)

--- .cmd
+++ .cmd
@@ -1,8 +1,8 @@
-autoflake --remove-all-unused-imports --remove-unused-variables --remove-duplicate-keys --in-place --recursive .
-isort .
-black .
 for /r %%f in (*.py) do (
     pyupgrade --py38-plus "%%f"
 )
+autoflake --remove-all-unused-imports --remove-unused-variables --remove-duplicate-keys --in-place --recursive .
+isort .
+black .
 coverage run -m unittest discover tests
 coverage xml
