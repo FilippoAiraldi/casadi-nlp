4553fa878a81ccd16712774c91ae5e73cba2802b
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-09-15 19:55:12 +0200

    implemented and tested the scenario mpc

--- src/csnlp/wrappers/__init__.py
+++ src/csnlp/wrappers/__init__.py
@@ -1,6 +1,14 @@
-__all__ = ["Wrapper", "NonRetroactiveWrapper", "NlpSensitivity", "Mpc", "NlpScaling"]
+__all__ = [
+    "Mpc",
+    "NlpScaling",
+    "NlpSensitivity",
+    "NonRetroactiveWrapper",
+    "ScenarioBasedMpc",
+    "Wrapper",
+]
 
-from csnlp.wrappers.mpc import Mpc
+from csnlp.wrappers.mpc.mpc import Mpc
+from csnlp.wrappers.mpc.scenario_based_mpc import ScenarioBasedMpc
 from csnlp.wrappers.scaling import NlpScaling
 from csnlp.wrappers.sensitivity import NlpSensitivity
 from csnlp.wrappers.wrapper import NonRetroactiveWrapper, Wrapper
