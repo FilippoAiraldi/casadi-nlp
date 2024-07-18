cca264930402021cdd59d5a48be0a9cd6908fcef
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-07-18 20:44:53 +0200

    small improv to docs again

--- src/csnlp/__init__.py
+++ src/csnlp/__init__.py
@@ -2,15 +2,15 @@ r"""**C**\ a\ **s**\ ADi-**NLP**\  (**csnlp**, for short) is a library that prov
 classes and utilities to model, solve and analyse nonlinear (but not only) programmes
 (NLPs) for optimization purposes.
 
-==================== ========================================================
+==================== =======================================================
 **Documentation**        https://casadi-nlp.readthedocs.io/en/latest/
 
 **Download**             https://pypi.python.org/pypi/csnlp/
 
 **Source code**          https://github.com/FilippoAiraldi/casadi-nlp/
 
-**Report issues**        https://github.com/FilippoAiraldi/casadi-nlp/issues/
-==================== ========================================================
+**Report issues**        https://github.com/FilippoAiraldi/casadi-nlp/issues
+==================== =======================================================
 """
 
 __version__ = "1.5.11.rc5"
