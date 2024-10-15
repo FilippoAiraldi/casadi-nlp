62f90688ba274e2bbbf5b1bcde449cd2b4e417f9
mallick <sam.mallick.97@gmail.com>
2024-10-15 14:29:01 +0200

    rename file to pwa_mpc
    (cherry picked from commit eef1b567676490769db925b09a6b8d63bb8fc23b)

--- src/csnlp/wrappers/__init__.py
+++ src/csnlp/wrappers/__init__.py
@@ -50,7 +50,7 @@ __all__ = [
 
 from .mpc.mpc import Mpc
 from .mpc.scenario_based_mpc import ScenarioBasedMpc
-from .mpc.hybrid_mpc import HybridMpc
+from .mpc.pwa_mpc import PwaMpc
 from .scaling import NlpScaling
 from .sensitivity import NlpSensitivity
 from .wrapper import NonRetroactiveWrapper, Wrapper
