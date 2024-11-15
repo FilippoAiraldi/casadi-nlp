21e56763f5bf02b43ad521d6fc6e922b601b7ce9
Filippo Airaldi <48798518+FilippoAiraldi@users.noreply.github.com>
2024-11-15 11:03:06 +0100

    Merge pull request #6 from SamuelMallick/switching_dynamics
    Further work to be done on main repo's experimental
    
    (cherry picked from commit 08204794b03881708e58963f797e43c70ed9f319)

--- src/csnlp/wrappers/mpc/pwa_mpc.py
+++ src/csnlp/wrappers/mpc/pwa_mpc.py
@@ -1,15 +1,23 @@
-from collections.abc import Sequence
+from collections.abc import Collection, Iterable, Sequence
 from dataclasses import dataclass
+from numbers import Integral
 from typing import Any, Literal, Optional, TypeVar, Union
 
 import casadi as cs
 import numpy as np
 import numpy.typing as npt
 
+from csnlp.core.solutions import Solution
+
 from ...core.data import find_index_in_vector
 from .mpc import Mpc
 
 SymType = TypeVar("SymType", cs.SX, cs.MX)
+
+
+def _n(parname: str, index: int) -> str:
+    """Internal utility for the naming convention of the ``i``-th region parameters."""
+    return f"{parname}__{index}"
 
 
 @dataclass
@@ -51,11 +59,22 @@ class PwaMpc(Mpc[SymType]):
             A_s x + B_s u + c_s & \text{if } S_s x + R_s u \leq T_s
         \end{cases}
 
-    Following :cite:`bemporad_control_1999`, the PWA dynamics can be converted to
-    mixed-logical dynamical form, and the ensuing MPC optimization becomes a
-    mixed-integer optimization problem. This is done under the hood via the
-    :meth:`set_pwa_dynamics` method. See also :cite:`borrelli_predictive_2017` for
-    further details.
+    This MPC controller class can then handle two different types of specifications for
+    the dynamics.
+
+    * **time-varying affine dynamics**: the MPC controller can be considered as a
+      linear MPC controller with time-varying dynamics, in which case the dynamics
+      must be defined via the :meth:`set_time_varying_affine_dynamics` method. Then,
+      prior to solving the optimization problem, the sequence of regions to be active
+      at each time-step needs to be set via :meth:`set_switching_sequence` by the
+      user/externally.
+    * **optimzing also the sequence**: alternatively, the sequence of PWA regions can
+      be optimized over, in which case the dynamics must be defined via the
+      :meth:`set_pwa_dynamics` method. Following :cite:`bemporad_control_1999`, the PWA
+      dynamics are converted to mixed-logical dynamical form, and the ensuing MPC
+      optimization becomes a mixed-integer optimization problem. This is done under the
+      hood via the :meth:`set_pwa_dynamics` method. See also
+      :cite:`borrelli_predictive_2017` for further details.
 
     Parameters
     ----------
@@ -83,9 +102,47 @@ class PwaMpc(Mpc[SymType]):
         Raises if the shooting method is invalid; or if any of the horizons are invalid;
         or if the number of scenarios is not a positive integer."""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._fixed_sequence_dynamics = False
+        self._pwa_system: Optional[Sequence[PwaRegion]] = None
+        self._sequence: Optional[Collection[int]] = None
+
+    def validate_pwa_dimensions(self, pwa_system: Iterable[PwaRegion]) -> None:
+        """Validates that the dimensions are correct for all matrices in
+        the passed PWA system.
+
+        Parameters
+        ----------
+        pwa_system : iterable of PwaRegion
+            An iterable of :class:`PwaRegion` objects, where the i-th object contains
+            the matrices defining the i-th region of the PWA system.
+
+        Raises
+        ------
+        ValueError
+            Raises if the dimensions of any matrix in any region do not match the
+            expected shape.
+        """
+        ns = self.ns
+        na = self.na
+        nsa = ns + na
+        n_ineq = pwa_system[0].T.shape[0]  # must be the same for all regions
+        for i, region in enumerate(pwa_system):
+            if region.A.shape != (ns, ns):
+                raise ValueError(f"A in region {i} must have shape ({ns}, {ns}).")
+            if region.B.shape != (ns, na):
+                raise ValueError(f"B in region {i} must have shape ({ns}, {na}).")
+            if region.c.shape != (ns,):
+                raise ValueError(f"c in region {i} must have shape ({ns},).")
+            if region.S.shape != (n_ineq, nsa):
+                raise ValueError(f"S in region {i} must have shape ({n_ineq}, {nsa}).")
+            if region.T.shape != (n_ineq,):
+                raise ValueError(f"T in region {i} must have shape ({n_ineq},).")
+
     def set_pwa_dynamics(
         self,
-        pwa_system: Sequence[PwaRegion],
+        pwa_system: Collection[PwaRegion],
         D: Union[npt.NDArray[np.floating], cs.DM],
         E: npt.NDArray[np.floating],
         clp_opts: Optional[dict[str, Any]] = None,
@@ -103,7 +160,7 @@ class PwaMpc(Mpc[SymType]):
         Parameters
         ----------
         pwa_system : collection of PwaRegion
-            A sequence of :class:`PwaRegion` objects, where the i-th object contains
+            A collection of :class:`PwaRegion` objects, where the i-th object contains
             the matrices defining the i-th region of the PWA system.
         D : array or casadi.DM of shape (n_ineq, ns + na)
             The (possibly sparse) matrix ``D`` defining the polytopic constraints on the
@@ -133,12 +190,14 @@ class PwaMpc(Mpc[SymType]):
 
         Notes
         -----
-        This function will raise an error if lower and upper bounds on the states and
-        actions are set. This is because these bounds should be instead specified via
-        the matrices ``D`` and ``E``. Moreover, this function only uses these matrices
-        for internal computations. The user should take care to impose the constraints
-        :math::math:`D [x^\top, u^\top]^\top \leq E` in the optimization problem, as
-        well as any other, via the :meth:`constraint` method.
+        When multiple states and/or control inputs are defined, these are concatenated
+        into the single vectors :math:`x` and :math:`u`, respectively. Also, this
+        function will raise an error if lower and upper bounds on any state or action
+        are set. This is because these bounds should be instead specified via the
+        matrices ``D`` and ``E``. Moreover, this function only uses these matrices for
+        internal computations, so the user should take care to impose the constraints
+        :math:`D [x^\top, u^\top]^\top \leq E` in the optimization problem, as well as
+        any other, via the :meth:`constraint` method.
         """
         if self._dynamics_already_set:
             raise RuntimeError("Dynamics were already set.")
@@ -151,8 +210,8 @@ class PwaMpc(Mpc[SymType]):
         ubu = self.ubx[idx].data
         if (~np.isneginf(lbu)).any() or (~np.isposinf(ubu)).any():
             raise RuntimeError(
-                "cannot set lower and upper bounds on the actions in PWA systems; use "
-                "arguments `D` and `E` of `set_pwa_dyanmics` instead"
+                "Cannot set lower and upper bounds on the actions in PWA systems; use "
+                "arguments `D` and `E` of `set_pwa_dyanmics` instead."
             )
         if self._is_multishooting:
             X = cs.vvcat(self._states.values())
@@ -161,26 +220,13 @@ class PwaMpc(Mpc[SymType]):
             ubx = self.ubx[idx].data
             if (~np.isneginf(lbx)).any() or (~np.isposinf(ubx)).any():
                 raise RuntimeError(
-                    "cannot set lower and upper bounds on the states in PWA systems; "
-                    "use arguments `D` and `E` of `set_pwa_dyanmics` instead"
+                    "Cannot set lower and upper bounds on the states in PWA systems; "
+                    "use arguments `D` and `E` of `set_pwa_dyanmics` instead."
                 )
 
         # validate dimensions
-        ns = self.ns
-        na = self.na
-        nsa = ns + na
-        n_ineq = pwa_system[0].T.shape[0]  # must be the same for all regions
-        for i, region in enumerate(pwa_system):
-            if region.A.shape != (ns, ns):
-                raise ValueError(f"A in region {i} must have shape ({ns}, {ns}).")
-            if region.B.shape != (ns, na):
-                raise ValueError(f"B in region {i} must have shape ({ns}, {na}).")
-            if region.c.shape != (ns,):
-                raise ValueError(f"c in region {i} must have shape ({ns},).")
-            if region.S.shape != (n_ineq, nsa):
-                raise ValueError(f"S in region {i} must have shape ({n_ineq}, {nsa}).")
-            if region.T.shape != (n_ineq,):
-                raise ValueError(f"T in region {i} must have shape ({n_ineq},).")
+        self.validate_pwa_dimensions(pwa_system)
+        nsa = self.ns + self.na
         n_ineq = E.shape[0]
         if D.shape != (n_ineq, nsa):
             raise ValueError(f"D must have shape ({n_ineq}, {nsa}).")
@@ -199,7 +245,7 @@ class PwaMpc(Mpc[SymType]):
 
     def _set_pwa_dynamics(
         self,
-        regions: Sequence[PwaRegion],
+        regions: Collection[PwaRegion],
         D: Union[npt.NDArray[np.floating], cs.DM],
         E: npt.NDArray[np.floating],
         clp_opts: dict[str, Any],
@@ -207,12 +253,6 @@ class PwaMpc(Mpc[SymType]):
         max_num_threads: int,
     ) -> None:
         """Internal utility to create PWA dynamics constraints."""
-        nr = len(regions)
-        n_ineq = regions[0].T.size
-        ns, na = regions[0].B.shape
-        nsa = ns + na
-        N = self._prediction_horizon
-
         # solve linear programs to determine bounds for big-M relaxations. These LPs are
         # solved parallelly for each region and for each inequality defining the region.
         D = cs.sparsify(D)  # can be sparse
@@ -226,6 +266,13 @@ class PwaMpc(Mpc[SymType]):
         T = np.asarray(T_)
         AB = np.vstack(AB_).T
         C = np.asarray(C_)
+
+        nr = len(regions)
+        n_ineq = r.T.size
+        ns, na = r.B.shape
+        nsa = ns + na
+        N = self._prediction_horizon
+
         lp = {"h": cs.Sparsity(nsa, nsa), "a": D.sparsity()}
         lpsolver = cs.conic("lpsolver", "clp", lp, clp_opts)
 
@@ -278,3 +325,169 @@ class PwaMpc(Mpc[SymType]):
         self.constraint("region", cs.vcat(region), "<=", 0)
         self.constraint("z_x_ub", cs.vcat(z_x_ub), "<=", 0)
         self.constraint("z_x_lb", cs.vcat(z_x_lb), ">=", 0)
+
+    def set_time_varying_affine_dynamics(self, pwa_system: Sequence[PwaRegion]) -> None:
+        r"""Sets the time-varying affine dynamics of the system for the MPC controller.
+        The possible values taken by the affine dynamics are defined by the sequence of
+        :class:`PwaRegion`.
+
+        Parameters
+        ----------
+        pwa_system : sequence of PwaRegion
+            A sequence of :class:`PwaRegion` objects, where the i-th object contains
+            the matrices defining the i-th region of the PWA system.
+
+        Raises
+        ------
+        RuntimeError
+            Raises if the dynamics were already set.
+        ValueError
+            Raises if the dimensions of any matrix in any region do not match the
+            expected shape.
+
+        """
+        if self._dynamics_already_set:
+            raise RuntimeError("Dynamics were already set.")
+
+        self.validate_pwa_dimensions(pwa_system)
+        N = self._prediction_horizon
+        ns = self.ns
+        na = self.na
+        nsa = ns + na
+        n_ineq = pwa_system[0].T.size
+
+        # parameters defining time-varying dynamics
+        A = [self.parameter(f"A[{k}]", (ns, ns)) for k in range(N)]
+        B = [self.parameter(f"B[{k}]", (ns, na)) for k in range(N)]
+        c = [self.parameter(f"c[{k}]", (ns, 1)) for k in range(N)]
+        S = [self.parameter(f"S[{k}]", (n_ineq, nsa)) for k in range(N)]
+        T = [self.parameter(f"T[{k}]", (n_ineq, 1)) for k in range(N)]
+
+        X = cs.vcat(self._states.values())
+        U = cs.vcat(self._actions_exp.values())
+
+        # set dynamics constraints and region constraints
+        if not self._is_multishooting:
+            raise NotImplementedError("Single shooting not implemented yet.")
+        xs_next = []
+        for k in range(N):
+            x_next = A[k] @ X[:, k] + B[k] @ U[:, k] + c[k]
+            xs_next.append(x_next)
+        self.constraint(
+            "region",
+            cs.diagcat(*S)
+            @ cs.vertcat(*[cs.vertcat(X[:, k], U[:, k]) for k in range(N)])
+            - cs.vertcat(*T),
+            "<=",
+            0,
+        )
+        self.constraint("dyn", cs.hcat(xs_next), "==", X[:, 1:])
+
+        self._pwa_system = pwa_system
+        self._dynamics_already_set = True
+        self._fixed_sequence_dynamics = True
+
+    def set_switching_sequence(self, sequence: Collection[int]) -> None:
+        """Sets the sequence of regions to be active at each time step along the MPC
+        prediction horizon. Then, when solved, the MPC optimization will only optimze
+        over the action and state trajectories, while enforcing the sequence of regions
+        visited by the states to be the one provided here.
+
+        Parameters
+        ----------
+        sequence : collection of int
+            A collection of integers representing the indices of the regions to be
+            active at each time step along the prediction horizon, i.e., the k-th entry
+            of this collection represents the index of the region the state must be at
+            time step ``k``.
+
+        Raises
+        ------
+        ValueError
+            Raises if dynamics have not been set via
+            :meth:`set_time_varying_affine_dynamics`; if the sequence is not the same
+            length as the prediction horizon; if the sequence does not contain integers;
+            if the sequence contains integers that exceed the number of PWA regions
+            specified via :meth:`set_time_varying_affine_dynamics`.
+
+        Notes
+        -----
+        For internal validation purposes, please call first
+        :meth:`set_time_varying_affine_dynamics` and only then call this method.
+        """
+        if not self._fixed_sequence_dynamics or self._pwa_system is None:
+            raise ValueError(
+                "Sequence can only be set if time-varying dynamics are used."
+            )
+        if len(sequence) != self._prediction_horizon:
+            raise ValueError("Length of sequence must match the prediction horizon")
+
+        N = len(self._pwa_system)
+        for i, idx in enumerate(sequence):
+            if not isinstance(idx, Integral):
+                raise ValueError(
+                    f"{i}-th element of the sequence is not an integer; got {idx}."
+                )
+            if not 0 <= idx < N:
+                raise ValueError(
+                    f"{i}-th element of the sequence must be in [0, N); got {idx}."
+                )
+        self._sequence = sequence
+
+    @staticmethod
+    def get_optimal_switching_sequence(
+        sol: Solution[SymType],
+    ) -> npt.NDArray[np.integer]:
+        """Returns the optimal switching sequence of regions for the state trajectory
+        along the prediction horizon, which can be extracted from the solution of the
+        corresponding mixed-integer optimization MPC problem (i.e., when the dynamics
+        are set via :meth:`set_pwa_dynamics`, which allows to optimize over the sequence
+        as well, though more computationally expensive).
+
+        Parameters
+        ----------
+        sol : Solution
+            An optimal solution of the mixed-integer PWA MPC problem.
+
+        Returns
+        -------
+        array of ints
+            An array of integers representing the indices of the regions that active at
+            each time step along the prediction horizon, i.e., the k-th entry of this
+            array represents the index of the region the predicted state is in at time
+            step ``k``.
+
+        Raises
+        ------
+        KeyError
+            Raises if the solution does not contain the variable ``delta``, e.g., the
+            solution was not obtained from a mixed-integer optimization problem, or the
+            dynamics were not set via :meth:`set_pwa_dynamics`.
+        """
+        return sol.vals["delta"].toarray().argmax(0)
+
+    def solve(
+        self,
+        pars: Optional[dict[str, npt.ArrayLike]] = None,
+        vals0: Optional[dict[str, npt.ArrayLike]] = None,
+    ) -> Solution[SymType]:
+        if self._fixed_sequence_dynamics:
+            regions = self._pwa_system
+            assert regions is not None, "PWA system should have been set!"
+            if self._sequence is None:
+                raise ValueError(
+                    "A sequence must be set via `set_switching_sequence` prior to "
+                    "solving the MPC because the dyanmics were set via "
+                    "`set_time_varying_affine_dynamics`. Use `set_pwa_dynamics` instead"
+                    " to optimize over the sequence as well."
+                )
+
+            if pars is None:
+                pars = {}
+            for k, idx in enumerate(self._sequence):
+                pars[_n("A", k)] = regions[idx].A
+                pars[_n("B", k)] = regions[idx].B
+                pars[_n("c", k)] = regions[idx].c
+                pars[_n("S", k)] = regions[idx].S
+                pars[_n("T", k)] = regions[idx].T
+        return self.nlp.solve(pars, vals0)
