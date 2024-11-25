ac7510623f27910962540af566d6748a3d566175
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-11-25 13:34:32 +0100

    new post release

--- src/csnlp/__init__.py
+++ src/csnlp/__init__.py
@@ -13,7 +13,7 @@ classes and utilities to model, solve and analyse nonlinear (but not only) progr
 ==================== =======================================================
 """
 
-__version__ = "1.6.3rc2"
+__version__ = "1.6.3.post1"
 
 __all__ = ["Nlp", "Solution", "multistart", "scaling"]
 
